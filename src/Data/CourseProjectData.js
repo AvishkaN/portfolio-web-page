e37a0614625f3719c7e2e7a08a83f72d7c29a2ba
NIRMITHA <avishkanirmitha@gmail.com>
2022-01-09 21:28:54 +0530

    bankist project image  added

--- src/Data/CourseProjectData.js
+++ src/Data/CourseProjectData.js
@@ -3,7 +3,7 @@ const CourseProjectDataObj=[
         id:'1'+Math.random(),
         name:"Bankist project",
         IsBuilding:false,
-        backgroundImage:"https://raw.githubusercontent.com/AvishkaN/portfolio-web-page/master/src/assets/img/1-EcommerceProjectImage.png",
+        backgroundImage:"https://raw.githubusercontent.com/AvishkaN/portfolio-web-page/master/src/assets/img/3BankistApp.png",
         description:"This project i build with udemy course",
         sections:[
             {
@@ -42,6 +42,55 @@ const CourseProjectDataObj=[
                 paragraph:"",
                 ProjectView:true,
                 ProjectViewLink:"https://bankist-app-udemy.netlify.app/",
+                
+            },
+      
+
+        ]
+    },
+    {
+        id:'1'+Math.random(),
+        name:"Bankist  landing page",
+        IsBuilding:false,
+        backgroundImage:"https://raw.githubusercontent.com/AvishkaN/portfolio-web-page/master/src/assets/img/3BankistApp.png",
+        description:"This project i build with udemy course",
+        sections:[
+            {
+                title:"What course I did ?",
+                list:[
+                    "The Complete JavaScript Course 2022 From Zero to Expert  (in udemy)",
+                ]
+            },
+              {
+                title:"Project Description",
+                list:[],
+                paragraph:"",
+                
+            },
+         
+            {
+                title:"Project Progress",
+                list:[],
+                // paragraph:"Lor",
+                showProgress:true,
+                ProgressPercentage:100,
+                
+            },
+ 
+            {
+                title:"Source File",
+                list:[],
+                paragraph:"",
+                sourcefile:true,
+                sourceFileLink:"https://github.com/AvishkaN/bankist-app-landing-page",
+                
+            },
+            {
+                title:"Porject View",
+                list:[],
+                paragraph:"",
+                ProjectView:true,
+                ProjectViewLink:"https://bankist-landing-page1.netlify.app/",
                 
             },
       
